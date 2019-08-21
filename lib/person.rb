5b8e229e4d584bcd182229a3c7755d6bbffdae73
Hector Rodriguez Duenas <h.rodri831@gmail.com>
2019-08-21 21:43:02 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,23 +1,3 @@
 Class Person
-  def initialize
-    @name 
-    @job
-  end
-  
-  def name=(new_name)
-    @name 
-  end 
-  
-  def name 
-    @name 
-  end 
-  
-  def job=(new_job)
-    @job = new_job
-  end
-  
-  def job 
-    @job
-  end 
   
 end
